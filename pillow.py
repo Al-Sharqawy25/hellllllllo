03293d96d86c899937260ec1db875634290b96d4
Mohamed <muhamed_elsharqawy@yahoo.com>
2023-09-20 23:49:42 +0300

    photoshop like

---
+++ pillow.py
@@ -0,0 +1,10 @@
+import PIL
+
+from PIL import Image, ImageFilter
+
+# img_path = Image.open('1632507145167.jpg')
+before = Image.open('1632507145167.jpg')
+# img_path.show()
+# img_path.save('programmer.png')
+after = before.filter(ImageFilter.BoxBlur(5))
+after.save('programmer.png')
