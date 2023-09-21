ddf1e91c2a8ed4db90dc2dc90c0a3ae29799f472
Mohamed <muhamed_elsharqawy@yahoo.com>
2023-09-21 22:47:25 +0300

    adding typescript

--- pillow.py
+++ pillow.py
@@ -7,5 +7,7 @@ before = Image.open('1632507145167.jpg')
 # img_path.show()
 # img_path.save('programmer.png')
 # after = before.filter(ImageFilter.BoxBlur(5))
-after = before.convert('L')
+after = before.convert('L')  # black and white
+# box = (100, 50, 1350, 1700)
+# after = before.crop(box)
 after.save('programmer.png')
